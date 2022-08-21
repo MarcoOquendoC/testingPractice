c4e6ffebd7c5ed9bb9ae4ad253f162171f03a65e
Marco Oquendo <marcooquendoc@gmail.com>
2022-08-20 21:03:26 -0500

    rename files

--- stringLength.js
+++
@@ -1,8 +0,0 @@
-function stringLength(string) {
-  let result = string.replaceAll(' ', '');
-  if(result.length < 1) return 'input has no characters'
-  if(result.length > 10) return 'input too long to be tested'
-  return result.length;
-}
-
-export default stringLength
